b7859a5f6c2be0b1754a569dd3dd0ffef80f806b
이재영 <ldc1000@naver.com>
2024-11-11 14:46:39 +0900

    init

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by t2023-m0072 on 11/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+}
+//미리보기
+#Preview("ViewController") {
+    ViewController()
+    
+}
---
+++ CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by t2023-m0072 on 11/11/24.
+//
+
+import Testing
+@testable import Calculator
+
+struct CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
