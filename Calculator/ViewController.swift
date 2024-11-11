20469f4c1193e2196b7e52946e7f99aad5d1bb32
이재영 <ldc1000@naver.com>
2024-11-11 15:53:04 +0900

    feat: inputTextView

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,11 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let inputTextLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        setUpUI()
+    }
+}
+
+//MARK: - Set Up UI
+extension ViewController {
+    private func setUpUI() {
+        self.view.backgroundColor = .black
+        setUpInputLabel()
+    }
+    
+    private func setUpInputLabel() {
+        let inputTextLabel = self.inputTextLabel
+        
+        self.view.addSubview(inputTextLabel)
+        
+        inputTextLabel.backgroundColor = .clear
+        inputTextLabel.font = .boldSystemFont(ofSize: 60)
+        inputTextLabel.textColor = .white
+        inputTextLabel.text = "12345"
+        inputTextLabel.textAlignment = .right
+        
+        inputTextLabel.translatesAutoresizingMaskIntoConstraints = false
+        let topConstraint = inputTextLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 300)
+        let leadingConstraint = inputTextLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 30)
+        let trailingConstraint = inputTextLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -30)
+        let heightConstraint = inputTextLabel.heightAnchor.constraint(equalToConstant: 100)
+        
+        NSLayoutConstraint.activate([topConstraint, leadingConstraint, trailingConstraint, heightConstraint])
     }
 }
 //미리보기
